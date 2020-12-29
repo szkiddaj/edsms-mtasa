ec08aa6900d12a3a7bff1b2c741c5947afed90d5
szkiddaj <38421816+szkiddaj@users.noreply.github.com>
2020-12-30 00:08:51 +0100

    Update edsms.php

--- src/web/edsms.php
+++ src/web/edsms.php
@@ -43,7 +43,7 @@
           $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
           $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         } catch (PDOException $e) {
-          echo "Sikertelen adatbázis csatlakozás! $e";
+          echo $errorMsg . ' #DB1';
           exit;
         }
 
